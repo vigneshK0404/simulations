7cb56142bd8f4b64c2af55201c1e7a0064e8c653
vigneshK0404 <vigneshwar.karthik04@gmail.com>
2026-08-19 23:50:06 -0400

    fixed coord system and completed base brute_force class; still need to fix energy conservation issue

---
+++ src/brute_force.h
@@ -0,0 +1,39 @@
+#include "num_integrator.h"
+#include "vec2.h"
+
+
+class brute_force
+{
+    public:
+	brute_force() = default;
+
+	void calculate_accel(std::vector<std::unique_ptr<particle>>& p_list)
+	{
+	    size_t p_size = p_list.size();
+
+	    for(size_t i = 0; i < p_size; ++i)
+	    {
+		vec2<double> pos_i = p_list[i]->pos;
+
+		for(size_t j = i+1; j < p_size; ++j)
+		{
+		    vec2<double> delta = p_list[j]->pos - pos_i; 
+		    vec2<double> force = delta / delta.mag();
+
+		    p_list[i]->acc += force;
+		    p_list[j]->acc -= force;
+
+		}
+	    }
+
+	}
+
+
+
+};
+
+
+
+
+
+
