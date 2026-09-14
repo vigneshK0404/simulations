fda5d6fa8649418bd559d20feeca58c2579f9b96
vigneshK0404 <vigneshwar.karthik04@gmail.com>
2026-09-13 22:44:04 -0400

    fixed mass issue added COM calc next will add accelerations

--- src/barnes_hut.cxx
+++ src/barnes_hut.cxx
@@ -1,16 +1,16 @@
 #include "barnes_hut.h"
 
 
-void barnes_hut::build_tree(std::unique_ptr<node> current_node, particle* par)
+void barnes_hut::build_tree(node* current_node, particle* par)
 {
     // base case
     if(!current_node->p && !current_node->TR && !current_node->TL && !current_node->BR && !current_node->BL)
     {
 	current_node->p = par;
 	current_node->total_mass = 1;
+	return;
     }
-    return;
-
+    
     //recursive case
     current_node->total_mass++;
     double node_X = current_node->center.X();
@@ -18,17 +18,26 @@ void barnes_hut::build_tree(std::unique_ptr<node> current_node, particle* par)
     double node_W = current_node->width;
     
     bool right = (par->pos.X()>=node_X);
-    bool up = (par->pos.Y()>=current_node->node_Y);
+    bool up = (par->pos.Y() >= node_Y);
+
+    if(current_node->p)
+    {
+	current_node->total_mass--;
+	cur_par = current_node->p;
+	current_node->p = nullptr;
+	build_tree(current_node, cur_par);    
+    }
+
 
     if(right&&up)
     {	
 	if(!current_node->TR)
 	{
-	    vec2<double> nc(node_X + node_W/2, node_Y + node_W/2);
+	    vec2<double> nc(node_X + node_W/4, node_Y + node_W/4);
 	    current_node->TR = std::make_unique<node>(nc,node_W/2);
 	}
 	
-	build_tree(current_node->TR, par);
+	build_tree(current_node->TR.get(), par);
 
     }
 
@@ -36,47 +45,62 @@ void barnes_hut::build_tree(std::unique_ptr<node> current_node, particle* par)
     {	
 	if(!current_node->TL)
 	{
-	    vec2<double> nc(node_X - node_W/2, node_Y + node_W/2);
+	    vec2<double> nc(node_X - node_W/4, node_Y + node_W/4);
 	    current_node->TL = std::make_unique<node>(nc,node_W/2);
 	}
 	
-	build_tree(current_node->TL, par);
+	build_tree(current_node->TL.get(), par);
     }
 
     if(right&&!up)
     {	
 	if(!current_node->BR)
 	{
-	    vec2<double> nc(node_X + node_W/2, node_Y - node_W/2);
+	    vec2<double> nc(node_X + node_W/4, node_Y - node_W/4);
 	    current_node->BR = std::make_unique<node>(nc,node_W/2);
 	}
 	
-	build_tree(current_node->BR, par);
+	build_tree(current_node->BR.get(), par);
     }
 
     if(!right&&!up)
     {	
 	if(!current_node->BL)
 	{
-	    vec2<double> nc(node_X - node_W/2, node_Y - node_W/2);
+	    vec2<double> nc(node_X - node_W/4, node_Y - node_W/4);
 	    current_node->BL = std::make_unique<node>(nc,node_W/2);
 	}
 	
-	build_tree(current_node->BL, par);
-    }
-
-
-
-    cur_par = current_node->p;
-    current_node->p = nullptr;
-
-    build_tree(current_node, cur_par);    
-
+	build_tree(current_node->BL.get(), par);
+    } 
 }
 
-vec2<double> barnes_hut::center_of_mass(std::unique_ptr<node> c_node)
+vec2<double> barnes_hut::center_of_mass(node* c_node)
 {
-    
+   //base case
+   vec2<double> c_com;
+   if(!c_node->TR && !c_node->TL && !c_node->BR && !c_node->BL)
+   {
+	c_node->COM = c_node->p->pos;
+	return c_node->COM; 
+   }
+
+   if(c_node->TR)
+       c_com += center_of_mass(c_node->TR.get());
+   
+   if(c_node->TL)
+       c_com += center_of_mass(c_node->TL.get());
+   
+   if(c_node->BR)
+       c_com += center_of_mass(c_node->BR.get());
+   
+   if(c_node->BL)
+       c_com += center_of_mass(c_node->BL.get());
+
+   c_node->COM = c_com/c_node->total_mass;
+
+   return c_node->COM*(c_node->total_mass);
+   
 }
 
 
--- src/barnes_hut.h
+++ src/barnes_hut.h
@@ -11,12 +11,12 @@ struct node
 
     vec2<double> center;
     double width;
-    double COM;
+    vec2<double> COM;
     double total_mass;
 
     particle* p;
     
-    node(vec2<double> c, double w) : center(c), width(w), COM(0), total_mass(0), TR(nullptr), TL(nullptr), BR(nullptr), BL(nullptr), p(nullptr),{}
+    node(vec2<double> c, double w) : center(c), width(w), total_mass(0), TR(nullptr), TL(nullptr), BR(nullptr), BL(nullptr), p(nullptr),{}
 };
 
 class quad_tree
@@ -26,8 +26,8 @@ class quad_tree
 
     public:
 	quad_tree() : root(nullptr){}
-	void build_tree(std::unique_ptr<node>, particle*);
-	vec2<double> center_of_mass(std:unique_ptr<node>);
+	void build_tree(node*, particle*);
+	vec2<double> center_of_mass(node*);
     	~quad_tree();
 
 
